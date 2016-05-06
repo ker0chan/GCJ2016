aed518864d8056f244c5812f5a9ddce60be097a5
dufrenb <julien1.dufrene@epitech.eu>
2016-05-07 01:58:57 +0200

    Rewards

---
+++ Attraction/Assets/Rewards.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rewards : MonoBehaviour {
+	
+	public GameObject[] rewards;
+
+	// Use this for initialization
+	void Start () {
+		Spawn ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (Transform t in transform) {
+			Vector3 pos = t.position;
+			if (pos.y < 0)
+				pos.y += 0.4f;
+			else {
+				pos.y -= 0.4f;
+			}
+			t.position = pos;
+		}
+	}
+
+	void Spawn() {
+		int rand = Random.Range(0, rewards.Length);
+		GameObject temp = (GameObject) Instantiate (rewards[rand]);
+		temp.transform.SetParent(transform, true);
+	}
+}
