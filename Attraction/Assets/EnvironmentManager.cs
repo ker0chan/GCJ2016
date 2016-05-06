f949a9398b5e2b7542360f0fc777a7c14a926c54
Dimitri Landais <landais.dimitri@gmail.com>
2016-05-06 22:43:25 +0200

    initial commit

---
+++ Attraction/Assets/EnvironmentManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnvironmentManager : MonoBehaviour {
+
+	public GameObject[] obstacles;
+	public Transform obstaclesContainer;
+
+
+	// Use this for initialization
+	void Start () {
+		Spawn();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach(Transform t in obstaclesContainer)
+		{
+			Vector3 obstaclePosition = t.position;
+			obstaclePosition.x -= 0.4f;
+			t.position = obstaclePosition;
+			if(t.position.x > 3.5f && t.position.x <= 3.9f)
+			{
+				Spawn();
+			}
+		}
+	}
+
+	void Spawn()
+	{
+		int randomObstacle = Random.Range(0, obstacles.Length);
+		GameObject obstacle = (GameObject) Instantiate (obstacles[randomObstacle]);
+		obstacle.transform.SetParent(obstaclesContainer, false);
+	}
+}
---
+++ Attraction/Assets/Player.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	float gravity = 0.15f;
+	int direction = -1;
+	float maxHeight = 5.0f;
+	float minHeight = -5.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 currentPosition = transform.position;
+
+
+		if(transform.position.y >= minHeight && transform.position.y <= maxHeight)
+		{
+			currentPosition.y += gravity * direction;
+		} else 
+		{
+			currentPosition.y = Mathf.Clamp(currentPosition.y, minHeight, maxHeight);
+			direction = -direction;
+		}
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			direction = -direction;
+		}
+
+		transform.position = currentPosition;
+	}
+}
