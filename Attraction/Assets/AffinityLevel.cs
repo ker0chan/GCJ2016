e0b652602c5cee9fd8943de1b0406f91b239b8c0
Dimitri Landais <landais.dimitri@gmail.com>
2016-05-07 12:13:57 +0200

    Gestion de l'affinité (côté narratif)
    Commandes "changeaffinity NAME [plus/minus] AMOUNT"
    + première interface placeholder

---
+++ Attraction/Assets/AffinityLevel.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AffinityLevel : MonoBehaviour {
+
+	public Image barImage;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void SetAmount(int amount)
+	{
+		// 1% = 3 pixels. C'est comme ça.
+		barImage.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, amount * 3.0f);
+	}
+}
---
+++ Attraction/Assets/AffinityManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AffinityManager : MonoBehaviour {
+
+	[System.Serializable]
+	public struct NPCAffinity {
+		public string name;
+		public int affinity;
+		public AffinityLevel level;
+	}
+
+	public NPCAffinity[] affinityLevels;
+
+	Dictionary<string, NPCAffinity> affinityLevelsDictionary = new Dictionary<string, NPCAffinity>();
+
+	// Use this for initialization
+	void Start () {
+		foreach(NPCAffinity a in affinityLevels)
+		{
+			affinityLevelsDictionary.Add(a.name, a);
+			AddAffinity(a.name, 0);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void AddAffinity(string name, int amount)
+	{
+		NPCAffinity a = affinityLevelsDictionary[name];
+		a.affinity = Mathf.Clamp(a.affinity + amount, 0, 100);
+		a.level.SetAmount(a.affinity);
+	}
+}
