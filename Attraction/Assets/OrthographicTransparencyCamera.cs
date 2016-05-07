bd582f42e0715b4baada2f6b4506e026a13c26c8
Dimitri Landais <landais.dimitri@gmail.com>
2016-05-07 11:01:15 +0200

    Fix camera orthographic/gestion des elements transparents

---
+++ Attraction/Assets/OrthographicTransparencyCamera.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OrthographicTransparencyCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Camera.main.transparencySortMode = TransparencySortMode.Orthographic;
+	}
+}
