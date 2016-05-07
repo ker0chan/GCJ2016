7052307d530b4b98eb172ef4321624009fead1f2
Dimitri Landais <landais.dimitri@gmail.com>
2016-05-07 07:02:44 +0200

    Collisions + Nouvel obstacle de test
    Créaation de l'obstacle, mise en place des colliders, ajout d'une reward
    placée dans l'obstacle, gestion des collisions Player/Obstacle

--- Attraction/Assets/Player.cs
+++ Attraction/Assets/Player.cs
@@ -10,24 +10,70 @@ public class Player : MonoBehaviour {
 	float maxHeight = 5.0f;
 	float minHeight = -5.0f;
 
+	Vector3 initialPosition;
+
+	public float invicibilityDuration;
+	float currentInvincibility = 0.0f;
+
 	// Use this for initialization
 	void Start () {
-
+		initialPosition = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//Variable temporaire
 		Vector3 currentPosition = transform.position;
 
-		velocity = Mathf.Clamp(velocity + (velocityStep * direction), -maxVelocity, maxVelocity);
+		if(isInvincible())
+		{
+			currentInvincibility -= Time.deltaTime;
 
+			//TODO : Ramener le vaisseau à sa position initiale progressivement ?
+			//TODO : Ajouter un effet visuel pour indiquer qu'on est invincible
+		}
+		//Vitesse
+		velocity = Mathf.Clamp(velocity + (velocityStep * direction), -maxVelocity, maxVelocity);
+		//Position
 		currentPosition.y = Mathf.Clamp(currentPosition.y + velocity*Time.deltaTime, minHeight, maxHeight);
 
+
+		//Changement de direction
 		if(Input.GetKeyDown(KeyCode.Space))
 		{
 			direction = -direction;
 		}
 
 		transform.position = currentPosition;
+	}
+
+	void OnTriggerEnter2D(Collider2D c)
+	{
+		if (c.GetComponent<Obstacle>() != null)
+		{
+			Hit();
+		}
+		if (c.GetComponent<Rewards>() != null)
+		{
+			//TODO
+		}
+	}
+
+	bool isInvincible()
+	{
+		return currentInvincibility > 0.0f;
+	}
+
+	void Hit()
+	{
+		Debug.Log("Hit");
+		if(isInvincible()) return;
+
+
+		currentInvincibility = invicibilityDuration;
+		transform.position = initialPosition;
+		direction = -1;
+		velocity = 0.0f;
+
 	}
 }
---
+++ Attraction/Assets/Reward.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Reward : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x <= -17.0f) {
+			Destroy (transform.gameObject);
+		} 
+	}
+}
