f9053a5558826769fef2dce41c668955148e660f
Dimitri Landais <landais.dimitri@gmail.com>
2016-05-07 07:14:03 +0200

    Cleanup/Commentaires sur le script Player

--- Attraction/Assets/Player.cs
+++ Attraction/Assets/Player.cs
@@ -4,16 +4,26 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
+	//Vitesse verticale maximale du joueur
 	public float maxVelocity;
+	//Incréments de vitesse
 	public float velocityStep;
-	public float velocity;
-	public int direction = -1;
+	//Vitesse actuelle
+	float velocity;
+	//Direction (1 = haut, -1 = bas)
+	int direction = -1;
+	//Limites de mouvement verticales du joueur
 	float maxHeight = 5.0f;
 	float minHeight = -5.0f;
 
+	//Position d'origine du vaisseau
 	Vector3 initialPosition;
+	//Position à laquelle le vaisseau a heurté un obstacle
+	Vector3 hitPosition;
 
+	//Durée d'invincibilité
 	public float invicibilityDuration;
+	//Temps d'invincibilité restant
 	float currentInvincibility = 0.0f;
 
 	int score = 0;
@@ -21,7 +31,10 @@ public class Player : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		//Mémorise la position de départ du joueur
 		initialPosition = transform.position;
+		//Initialise le texte du score
+		UpdateScore();
 	}
 	
 	// Update is called once per frame
@@ -51,13 +64,16 @@ public class Player : MonoBehaviour {
 		transform.position = currentPosition;
 	}
 
+	//Détection des collisions
 	void OnTriggerEnter2D(Collider2D c)
 	{
+		//Obstacle
 		if (c.GetComponent<Obstacle>() != null)
 		{
 			Hit();
 		}
 
+		//Reward
 		Reward pickedUpReward = c.GetComponent<Reward>();
 		if (pickedUpReward != null)
 		{
@@ -65,30 +81,38 @@ public class Player : MonoBehaviour {
 		}
 	}
 
+	//Le joueur est-il invincible en ce moment ?
 	bool isInvincible()
 	{
 		return currentInvincibility > 0.0f;
 	}
 
+	//Obstacle touché
 	void Hit()
 	{
-		Debug.Log("Hit");
+		//Invincibilité => on quitte la fonction
 		if(isInvincible()) return;
 
-
+		//On met le timer d'invincibilité au max
 		currentInvincibility = invicibilityDuration;
+
+		//On reset la position du joueur, ainsi que sa vitesse et sa direction
 		transform.position = initialPosition;
 		direction = -1;
 		velocity = 0.0f;
 	}
 
+	//Ramassage de ressource
 	void PickUp(Reward reward)
 	{
+		//Notifie la reward qu'elle a été ramassée
 		reward.PickUp();
+		//Augmente le score
 		score += 1;
 		UpdateScore();
 	}
 
+	//Affiche le score courant dans la zone de texte "scoreText"
 	void UpdateScore()
 	{
 		scoreText.text = score.ToString();
