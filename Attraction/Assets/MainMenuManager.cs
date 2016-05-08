48ad4c2e813c3ae8da876b0269d8e1bb8c53b133
Dimitri Landais <landais.dimitri@gmail.com>
2016-05-08 13:07:32 +0200

    Menu principal

---
+++ Attraction/Assets/MainMenuManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainMenuManager : MonoBehaviour {
+
+	public Button button;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Play()
+	{
+		button.interactable = false;
+		UnityEngine.SceneManagement.SceneManager.LoadScene("narrative");
+	}
+}
+
